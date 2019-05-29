58b2cae3e8382f79499af41096a173f00078e2cb
seppe_verhavert <seppe.verhavert@gmail.com>
2019-05-29 15:31:09 +0200

    add clock

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function checkEverything() {
     checkDate();
+    checkTime();
 }
 
 function checkDate() {
@@ -14,5 +15,26 @@ function checkDate() {
     weekday[6] = "Saturday";
 
     var n = weekday[d.getDay()];
-    document.getElementById("openclosed").innerHTML = n;
+    document.getElementById("ShowDay").innerHTML = n;
+}
+
+function checkTime ( ) {
+  var currentTime = new Date ( );
+
+  var currentHours = currentTime.getHours ( );
+  var currentMinutes = currentTime.getMinutes ( );
+  var currentSeconds = currentTime.getSeconds ( );
+
+  currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+  currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+  var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+
+  currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+
+  currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+
+  var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+  
+  document.getElementById("clock").firstChild.nodeValue = currentTimeString;
 }
