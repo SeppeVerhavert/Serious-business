a8871ecaafbc4588aa2e0896bfe20aed6dd017a4
seppe_verhavert <seppe.verhavert@gmail.com>
2019-05-29 16:25:27 +0200

    put checkOpen in secondary.js

--- main.js
+++ main.js
@@ -38,13 +38,11 @@ function checkOpen() {
    
     if (n == Monday) {
         document.getElementById("ShowOpen").innerHTML = "Sorry, we are closed.";
+    } else if (n == Sunday && currentHours > 9 && currentHours < 15) {
+        document.getElementById("ShowOpen").innerHTML = "We are open!";
+    } else if (currentHours > 8 && currentHours < 16){
+        document.getElementById("ShowOpen").innerHTML = "We are open!";
     } else {
-        if (n == Sunday && currentHours > 9 && currentHours < 15){
-            document.getElementById("ShowOpen").innerHTML = "We are open!";
-        } else if (currentHours > 8 && currentHours < 16){
-            document.getElementById("ShowOpen").innerHTML = "We are open!";
-        } else {
-            document.getElementById("ShowOpen").innerHTML = "Sorry, we are closed.";
-        }
+        document.getElementById("ShowOpen").innerHTML = "Sorry, we are closed.";
     }
-}
+    }
