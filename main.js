99290ad659945a7383dd86a14f8c0cc9aa9ad2ab
seppe_verhavert <seppe.verhavert@gmail.com>
2019-06-03 16:09:22 +0200

    add global variables

--- main.js
+++ main.js
@@ -1,49 +1,42 @@
+// ==================
+// Global Viariables
+// ==================
+
+var d = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
+
+var t = new Date ( );
+
+var currentHours = t.getHours ( );
+var currentMinutes = t.getMinutes ( );
+var currentSeconds = t.getSeconds ( );
+
+currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+var t = ( currentHours < 12 ) ? "AM" : "PM";
+currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+
+var tstring = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + t;
+
+// ==================
+// Functions
+// ==================
+
 function checkDate() {
-
-  var d = new Date();
-  var weekday = new Array(7);
-  weekday[0] = "Sunday";
-  weekday[1] = "Monday";
-  weekday[2] = "Tuesday";
-  weekday[3] = "Wednesday";
-  weekday[4] = "Thursday";
-  weekday[5] = "Friday";
-  weekday[6] = "Saturday";
-
- var n = weekday[d.getDay()];
-
   document.getElementById("ShowDay").innerHTML = n;
 }
 
 function checkTime() {
-  var currentTime = new Date ( );
-
-  var currentHours = currentTime.getHours ( );
-  var currentMinutes = currentTime.getMinutes ( );
-  var currentSeconds = currentTime.getSeconds ( );
-
-  currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-  currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
-
-  var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
-
-  currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-
-  currentHours = ( currentHours == 0 ) ? 12 : currentHours;
-
-  var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
-
   document.getElementById("clock").innerHTML = currentTimeString;
-}
-
-function checkOpen() {
-  
-  var c = weekday[b.getDay()];
-
-  document.getElementById("clock").innerHTML = currentTimeString;
-    
-  if (n == "Monday") {
-    document.getElementById("OpenClosed").innerHTML = "Sorry we are closed.";
-  } 
-  
 }
