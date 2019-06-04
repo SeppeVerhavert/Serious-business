9110b10a06ad2346ff13e56bbb9dea976fac0055
seppe_verhavert <seppe.verhavert@gmail.com>
2019-06-04 12:01:33 +0200

    moved clock variables back to local var

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// =================
+// Global Variables
+// =================
+
 var now = new Date();
 var weekday = new Array(7);
 weekday[0] = "Sunday";
@@ -8,60 +12,57 @@ weekday[4] = "Thursday";
 weekday[5] = "Friday";
 weekday[6] = "Saturday";
 
-var checkTime = function() {
-  var today = weekday[now.getDay()];
-  var timeDiv = document.getElementById('timeDiv');
-  var dayOfWeek = now.getDay();
-  var hour = now.getHours();
-  var minutes = now.getMinutes();
-  var seconds = now.getSeconds();
+var today = weekday[now.getDay()];
+var dayOfWeek = now.getDay();
 
-  //add AM or PM
-  var suffix = hour >= 12 ? "PM" : "AM";
+var hours = now.getHours();
+var minutes = now.getMinutes();
+var seconds = now.getSeconds();
+var suffix = hours >= 12 ? "PM" : "AM";
 
-  // add 0 to one digit minutes
-  if (minutes < 10) {
-    minutes = "0" + minutes
-  };
+if (minutes < 10) {
+  minutes = "0" + minutes
+};
 
-  if (seconds < 10) {
-    seconds = "0" + seconds
-  };
+if (seconds < 10) {
+  seconds = "0" + seconds
+};
 
-  if (dayOfWeek == 1 ) {
-    dayDiv.innerHTML = 'It\'s ' + today
-    timeDiv.innerHTML =  hour + ':' + minutes + ':' + seconds + ' ' + suffix;
-    openDiv.innerHTML = 'Sorry, we\'re closed'
-    timeDiv.className = 'closed';
-  } 
+// =================
+// Functions
+// =================
 
-  else if ((dayOfWeek == 0) && hour >= 9 && hour <= 15) {
-    dayDiv.innerHTML = 'It\'s ' + today
-    timeDiv.innerHTML =  hour + ':' + minutes + ':' + seconds + ' ' + suffix;
+function checkDay() {
+  dayDiv.innerHTML = 'It\'s ' + today
+}
+
+function checkTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = addZero(m);
+  s = addZero(s);
+  document.getElementById('timeDiv').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(checkTime, 1000);
+}
+function addZero(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+function checkOpen() {
+
+  if ((dayOfWeek == 0) && hours >= 9 && hours <= 15) {
     openDiv.innerHTML = 'Hooray, we\'re open!'
-    timeDiv.className = 'open';
   } 
   
-  else if ((dayOfWeek == 2 || dayOfWeek == 3 || dayOfWeek == 4 || dayOfWeek == 5) && hour >= 8 && hour <= 16) {
-    dayDiv.innerHTML = 'It\'s ' + today
-    timeDiv.innerHTML =  hour + ':' + minutes + ':' + seconds + ' ' + suffix;
+  else if ((dayOfWeek == 2 || dayOfWeek == 3 || dayOfWeek == 4 || dayOfWeek == 5) && hours >= 8 && hours <= 16) {
     openDiv.innerHTML = 'Hooray, we\'re open!'
-    timeDiv.className = 'open';
   } 
   
   else {
-    if (hour == 0 || hour > 12) {
-    }
-    dayDiv.innerHTML = 'It\'s ' + today
-    timeDiv.innerHTML =  hour + ':' + minutes + ':' + seconds + ' ' + suffix;
     openDiv.innerHTML = 'Sorry, we\'re closed!'
-    timeDiv.className = 'closed';
   }
 };
-
-var currentDay = weekday[now.getDay()];
-var currentDayID = "#" + currentDay; //gets todays weekday and turns it into id
-$(currentDayID).toggleClass("today"); //hightlights today in the view hours modal popup
-
-setInterval(checkTime, 1000);
-checkTime();
